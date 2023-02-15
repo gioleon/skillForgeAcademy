5f08bdaa2cf624f80647a1298934a2aeb30c4233
gioleon <giovannileon2017@gmail.com>
2023-02-15 15:45:24 -0500

    creation of empty based projects for backend and frontend

---
+++ backend/noname/src/main/java/com/noname/NonameApplication.java
@@ -0,0 +1,13 @@
+package com.noname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonameApplication.class, args);
+	}
+
+}
---
+++ backend/noname/src/test/java/com/noname/NonameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
