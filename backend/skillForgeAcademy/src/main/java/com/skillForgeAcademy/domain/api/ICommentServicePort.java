002809ba8100bec5d7a7f3a5558bd68a291e03d7
Gabriel Sabatini <gabrielsabatini16gmail.com>
2023-04-12 21:01:45 -0500

    Creation of Api/Persistence/Usecase

---
+++ backend/skillForgeAcademy/src/main/java/com/skillForgeAcademy/domain/api/ICommentServicePort.java
@@ -0,0 +1,7 @@
+package com.skillForgeAcademy.domain.api;
+
+import com.skillForgeAcademy.domain.model.CommentModel;
+import com.skillForgeAcademy.domain.utility.GenericService;
+
+public interface ICommentServicePort extends GenericService<CommentModel,Long> {
+}
