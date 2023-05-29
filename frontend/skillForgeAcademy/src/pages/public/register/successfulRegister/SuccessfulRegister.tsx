2b729a55ead6f3f7195e75cd4bc30c63a74e89a1
RoynerG <roynergh@hotmail.com>
2023-05-29 11:13:40 -0500

    update: styled of verification and successfullregister

--- frontend/skillForgeAcademy/src/pages/public/register/successfulRegister/SuccessfulRegister.tsx
+++ frontend/skillForgeAcademy/src/pages/public/register/successfulRegister/SuccessfulRegister.tsx
@@ -3,11 +3,20 @@ import { useLocation, useParams } from "react-router-dom";
 function SuccessfulRegister() {
   // use useLocation to get the state sent by register page.
   const location = useLocation();
+  const titulo = "¡Hola!👋";
 
   return (
-    <div>
-      Bienvenido a Skill Forge Academy! a su correo <b>{location.state.email}</b> le hemos
-      enviado un enlace para que active su cuenta
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">{titulo}</h1>
+          <p className="py-6 text-left">
+            Bienvenido a Skill Forge Academy! a su correo
+            <b className="before:content-['📤']">{location.state.email}</b> le
+            hemos enviado un enlace para que active su cuenta
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/skillForgeAcademy/tailwind.config.js
+++ frontend/skillForgeAcademy/tailwind.config.js
@@ -4,5 +4,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("flowbite/plugin")],
+  plugins: [require("daisyui")],
 };
